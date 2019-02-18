722327a1ece093ca033fb256a76576634dcb86f1
NaynaPriyadarshi <you@example.com>
2019-02-18 16:21:55 +1000

    init files

---
+++ Program2_PrimeNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2_PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            Console.WriteLine("Print Numbers :  ");
+
+            for (int i = 2; i <= 100; i++)
+            {
+
+                for (int j = 2; j <= 100; j++)
+
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+
+                    }
+
+                }
+                if (isPrime)
+                {
+
+                    Console.WriteLine("\t" + i);
+                }
+
+
+                isPrime = true;
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
